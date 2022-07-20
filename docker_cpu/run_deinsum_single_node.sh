7b7c3caa0b7e3472f7dc19763dfbcb591526c238
Alexandros Nikolaos Ziogas <alexandros.ziogas@inf.ethz.ch>
2022-07-20 21:27:31 +0200

    Updated CPU artifact.

---
+++ docker_cpu/run_deinsum_single_node.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [[ $# -ge 1 ]]
+then
+    num_nodes=$1
+else
+    num_nodes=1
+fi
+
+./compile_hptt.sh
+cd /storage/results
+libiomp_path=/opt/intel/oneapi/compiler/2021.3.0/linux/compiler/lib/intel64_lin
+current_path=$LIBRARY_PATH
+current_ldpath=$LD_LIBRARY_PATH
+export LIBRARY_PATH="$LIBRARY_PATH:$MKLROOT/lib/intel64"
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$MKLROOT/lib/intel64:$libiomp_path:$HPTT_ROOT/lib"
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$MKLROOT/lib/intel64:$HPTT_ROOT/lib"
+for prog in "mm" "mttkrp_order_3" "mttkrp_order_5" "ttmc"
+do
+    echo "Running ${prog} CPU testing with ${num_nodes} MPI processes."
+    mpirun -n ${num_nodes} python3 /dace/samples/distributed/${prog}_testing.py
+done
+export LIBRARY_PATH=$current_path
+export LD_LIBRARY_PATH=$current_ldpath
